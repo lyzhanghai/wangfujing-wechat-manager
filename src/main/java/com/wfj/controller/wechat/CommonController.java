ed9ab43115c6d0f5af0c58e60616074179981d11
kongqingfu <kongqingfu@wangfujing.com>
2016-11-22 14:39:03 +0800

    commonController

---
+++ src/main/java/com/wfj/controller/wechat/CommonController.java
@@ -0,0 +1,65 @@
+package com.wfj.controller.wechat;
+
+import com.wfj.dto.AccessTokenDto;
+import com.wfj.util.WechatUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/common")
+public class CommonController {
+
+    @Autowired
+    private WechatUtil util;
+
+    @RequestMapping(value = "memberInfo", method = RequestMethod.GET)
+    public void memberInfo(HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam(value = "code", required = true) String code,
+                           @RequestParam(value = "state", required = true) String state) {
+        long starTime = System.currentTimeMillis();
+        try {
+            // 1 通过门店接口获取appID,appSecret
+            //StoreInfoDto storeInfo = util.getStoreInfo(state);
+            //System.out.println("storeInfo ================ " + storeInfo);
+            // 2 通过appID,appSecret获取access_token
+            String accessToken = util.getAccessToken("wx7aec942c6742752d", "c27b7472a3bb1e9874c240a681b87880");
+            System.out.println("accessToken ================ " + accessToken);
+            // 3 通过code或取网页授权access_token(暂时不用)及openID
+            AccessTokenDto atkDto = util.getOpenId("wx7aec942c6742752d", "c27b7472a3bb1e9874c240a681b87880",code);
+            System.out.println("atkDto ================ " + atkDto);
+            //// 4 通过access_token,openID获取用户信息
+            //MemberInfo memberInfo = util.Openid_userinfo(atkDto.getOpenid(), storeInfo.getAppId(),
+            //        storeInfo.getSecret());
+            //System.out.println("memberInfo ================ " + memberInfo);
+            //String name = util.getURLEncoder(memberInfo.getNickname());
+            //String para = "&appId=" + storeInfo.getAppId() + "&openId=" + atkDto.getOpenid()
+            //        + "&headimgurl=" + memberInfo.getHeadimgurl() + "&nickname=" + name
+            //        + "&registType=" + storeInfo.getIsChannel() + "&uid=" + memberInfo.getUnionid()
+            //        + "&storeCode=" + storeInfo.getStoreCode();
+            //response.setHeader("Content-type", "text/html;charset=UTF-8");
+            //response.setCharacterEncoding("UTF-8");
+            //// Map<String, String> paramMap = new HashMap<String, String>();
+            //// paramMap.put("appid", storeInfo.getAppId());
+            //// paramMap.put("openid", atkDto.getOpenid());
+            //String sendGet = getMemberInfo(storeInfo.getAppId(), atkDto.getOpenid());// HttpUtil.sendGet("",
+            //// paramMap);
+            //// String sendGet = getMemberInfo(storeInfo.getAppId(),
+            //// atkDto.getOpenid());
+            //long endTime = System.currentTimeMillis();
+            //System.out.println("------------------------------------" + (endTime - starTime));
+            //response.sendRedirect(sendGet + para);
+            //System.out.println("decoder ================ " + sendGet + para);
+            //PrintWriter out = response.getWriter();
+            //out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/main/java/com/wfj/util/WechatUtil.java
+++ src/main/java/com/wfj/util/WechatUtil.java
@@ -1,88 +1,88 @@
 package com.wfj.util;
 
+import com.wfj.dto.AccessTokenDto;
+import net.sf.json.JSONObject;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
-
-import net.sf.json.JSONObject;
-
 public class WechatUtil {
-	private Logger logger = Logger.getLogger(WechatUtil.class);
+    private Logger logger = Logger.getLogger(WechatUtil.class);
 
-	// @Autowired
-	// private RedisUtil redisUtil;
+    @Autowired
+    private RedisUtil redisUtil;
 
-	/**
-	 * 获取access_token
-	 *
-	 * @Methods Name tokenInit
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param appid
-	 * @param secret
-	 * @return String
-	 */
-	public String tokenInit(String appid, String secret) {
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("grant_type", "client_credential");
-		params.put("appid", appid);
-		params.put("secret", secret);
-		String jsontoken = null;
-		String accessToken = null;
-		try {
-			// jsontoken =
-			// com.wfj.util.HttpUtil.sendGet(com.wfj.util.Constants.ACCESSTOKENURL,
-			// params);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    /**
+     * 获取access_token
+     *
+     * @param appid
+     * @param secret
+     * @return String
+     * @Methods Name tokenInit
+     * @Create In 2016年9月26日 By kongqf
+     */
+    public String tokenInit(String appid, String secret) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("grant_type", "client_credential");
+        params.put("appid", appid);
+        params.put("secret", secret);
+        String jsontoken = null;
+        String accessToken = null;
+        try {
+            jsontoken =
+                    com.wfj.util.HttpUtil.sendGet(PropertiesUtils.findPropertiesKey("accessTokenUrl"), params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		JSONObject jsonObject = JSONObject.fromObject(jsontoken);
-		if (jsonObject.has("access_token")) {
-			accessToken = jsonObject.getString("access_token");
-		}
+        JSONObject jsonObject = JSONObject.fromObject(jsontoken);
+        if (jsonObject.has("access_token")) {
+            accessToken = jsonObject.getString("access_token");
+        }
 
-		// boolean flag = redisUtil.setKey(appid, accessToken, 7000);
-		// if (!flag) {
-		// logger.error("tokenInit redis save:" + flag);
-		// }
-		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
-				+ "token 为==============================" + accessToken);
-		return accessToken;
-	}
+        boolean flag = redisUtil.setKey(appid, accessToken, 7000);
+        if (!flag) {
+            logger.error("tokenInit redis save:" + flag);
+        }
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
+                + "token 为==============================" + accessToken);
+        return accessToken;
+    }
 
-	/**
-	 * 获取access_token
-	 *
-	 * @Methods Name getAccessToken
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param appid
-	 * @param secret
-	 * @return String
-	 */
-	public String getAccessToken(String appid, String secret) {
-		String accessToken = null;
-		// accessToken = redisUtil.getKey(appid, "0000");
-		if ("0000".equals(accessToken)) {
-			accessToken = tokenInit(appid, secret);
-		}
-		return accessToken;
-	}
+    /**
+     * 获取access_token
+     *
+     * @param appid
+     * @param secret
+     * @return String
+     * @Methods Name getAccessToken
+     * @Create In 2016年9月26日 By kongqf
+     */
+    public String getAccessToken(String appid, String secret) {
+        String accessToken = null;
+        accessToken = redisUtil.getKey(appid, "0000");
+        if ("0000".equals(accessToken)) {
+            accessToken = tokenInit(appid, secret);
+        }
+        return accessToken;
+    }
 
-	/**
-	 * 获取微信用户信息
-	 *
-	 * @Methods Name Openid_userinfo
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param openid
-	 * @return
-	 * @throws Exception
-	 *             MemberInfo
-	 */
-	/*
-	 * public MemberInfo Openid_userinfo(String openid, String appid, String
+    /**
+     * 获取微信用户信息
+     *
+     * @Methods Name Openid_userinfo
+     * @Create In 2016年9月26日 By kongqf
+     * @param openid
+     * @return
+     * @throws Exception
+     *             MemberInfo
+     */
+    /*
+     * public MemberInfo Openid_userinfo(String openid, String appid, String
 	 * secret) throws Exception { HashMap<String, String> params = new
 	 * HashMap<String, String>(); params.put("access_token",
 	 * getAccessToken(appid, secret)); params.put("openid", openid);
@@ -107,84 +107,88 @@ public class WechatUtil {
 	 * errorCode + " errmg:" + errorMsg); } } } return memberInfo; }
 	 */
 
-	/**
-	 * 获取openid
-	 *
-	 * @Methods Name getOpenId
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param appid
-	 * @param secret
-	 * @param code
-	 * @return AccessTokenDto
-	 */
-	/*
-	 * public AccessTokenDto getOpenId(String appid, String secret, String code)
-	 * { AccessTokenDto accessTokenDto = new AccessTokenDto(); HashMap<String,
-	 * String> params = new HashMap<String, String>(); params.put("appid",
-	 * appid); params.put("secret", secret); params.put("code", code);
-	 * params.put("grant_type", "authorization_code"); try { String openIdInfo =
-	 * com.wfj.util.HttpUtil.sendGet(com.wfj.util.Constants.GETOPENIDURL,
-	 * params); accessTokenDto = com.wfj.util.JsonUtil.getDTO(openIdInfo,
-	 * AccessTokenDto.class); } catch (Exception e) { }
-	 * 
-	 * return accessTokenDto; }
-	 */
+    /**
+     * 获取openid
+     *
+     * @param appid
+     * @param secret
+     * @param code
+     * @return AccessTokenDto
+     * @Methods Name getOpenId
+     * @Create In 2016年9月26日 By kongqf
+     */
 
-	/**
-	 * BASE64编码
-	 *
-	 * @Methods Name getEncoder
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param str
-	 * @return String
-	 */
-	/*
-	 * public String getEncoder(String str) { String s = ""; BASE64Encoder
+    public AccessTokenDto getOpenId(String appid, String secret, String code) {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("appid", appid);
+        params.put("secret", secret);
+        params.put("code", code);
+        params.put("grant_type", "authorization_code");
+        try {
+            String openIdInfo = com.wfj.util.HttpUtil.sendGet(PropertiesUtils.findPropertiesKey("openidUrl"), params);
+            accessTokenDto = com.wfj.util.JsonUtil.getDTO(openIdInfo, AccessTokenDto.class);
+        } catch (Exception e) {
+        }
+
+        return accessTokenDto;
+    }
+
+    /**
+     * BASE64编码
+     *
+     * @Methods Name getEncoder
+     * @Create In 2016年9月26日 By kongqf
+     * @param str
+     * @return String
+     */
+    /*
+     * public String getEncoder(String str) { String s = ""; BASE64Encoder
 	 * encoder = new BASE64Encoder(); try { s =
 	 * encoder.encode(str.getBytes("UTF-8")); } catch
 	 * (UnsupportedEncodingException e) { logger.info("encoder:" + str + e); }
 	 * return s; }
 	 */
 
-	/**
-	 * BASE64解码
-	 *
-	 * @Methods Name getDecoder
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param str
-	 * @return String
-	 */
-	/*
-	 * public String getDecoder(String str) { String s = ""; BASE64Decoder
+    /**
+     * BASE64解码
+     *
+     * @Methods Name getDecoder
+     * @Create In 2016年9月26日 By kongqf
+     * @param str
+     * @return String
+     */
+    /*
+     * public String getDecoder(String str) { String s = ""; BASE64Decoder
 	 * decoder = new BASE64Decoder(); byte[] bytes; try { bytes =
 	 * decoder.decodeBuffer(str); s = new String(bytes, "UTF-8"); } catch
 	 * (IOException e) { logger.info("decoder:" + str + e); } return s; }
 	 */
 
-	/**
-	 * URL 编码
-	 *
-	 * @Methods Name getURLEncoder
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param str
-	 * @return String
-	 */
-	/*
-	 * public String getURLEncoder(String str) { try { str =
+    /**
+     * URL 编码
+     *
+     * @Methods Name getURLEncoder
+     * @Create In 2016年9月26日 By kongqf
+     * @param str
+     * @return String
+     */
+    /*
+     * public String getURLEncoder(String str) { try { str =
 	 * URLEncoder.encode(str, "UTF-8"); } catch (UnsupportedEncodingException e)
 	 * { logger.info("URLEncoder:" + str + e); } return str; }
 	 */
 
-	/**
-	 * URL解码
-	 *
-	 * @Methods Name getURLDecoder
-	 * @Create In 2016年9月26日 By kongqf
-	 * @param str
-	 * @return String
-	 */
-	/*
-	 * public String getURLDecoder(String str) { try { str =
+    /**
+     * URL解码
+     *
+     * @Methods Name getURLDecoder
+     * @Create In 2016年9月26日 By kongqf
+     * @param str
+     * @return String
+     */
+    /*
+     * public String getURLDecoder(String str) { try { str =
 	 * URLDecoder.decode(str, "UTF-8"); } catch (UnsupportedEncodingException e)
 	 * { logger.info("URLDecoder:" + str + e); } return str; }
 	 */
