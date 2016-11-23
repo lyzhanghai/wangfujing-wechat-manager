c8b28c65bef45d612801935e9c00af0e00e75fb9
kongqingfu <kongqingfu@wangfujing.com>
2016-11-23 13:51:10 +0800

    commonController

--- src/main/java/com/wfj/controller/wechat/CommonController.java
+++ src/main/java/com/wfj/controller/wechat/CommonController.java
@@ -2,6 +2,7 @@ package com.wfj.controller.wechat;
 
 import com.wfj.dto.AccessTokenDto;
 import com.wfj.dto.MemberInfo;
+import com.wfj.message.req.StoreInfoDto;
 import com.wfj.util.PropertiesUtils;
 import com.wfj.util.WechatUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,10 +29,10 @@ public class CommonController {
         long starTime = System.currentTimeMillis();
         try {
             // 1 通过门店接口获取appID,appSecret
-            // StoreInfoDto storeInfo = util.getStoreInfo(state);
-            // System.out.println("storeInfo ================ " + storeInfo);
-            String appid = "wx871d0104ae72e615";
-            String secret = "00e66c2772af76181745b6f5d92b5801";
+             StoreInfoDto storeInfo = util.getStoreInfo(state);
+             System.out.println("storeInfo ================ " + storeInfo);
+            String appid =storeInfo.getAppId(); //"wx871d0104ae72e615";
+            String secret =storeInfo.getSecret(); //"00e66c2772af76181745b6f5d92b5801";
             // 2 通过appID,appSecret获取access_token
             String accessToken = util.getAccessToken(appid, secret);
             System.out.println("accessToken ================ " + accessToken);
