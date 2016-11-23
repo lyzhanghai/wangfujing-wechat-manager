57e4bca25ae8f86393a02c660114faf261941642
kongqingfu <kongqingfu@wangfujing.com>
2016-11-23 11:18:43 +0800

    commonController

--- src/main/java/com/wfj/controller/wechat/test.java
+++ src/main/java/com/wfj/controller/wechat/test.java
@@ -1,21 +1,16 @@
 package com.wfj.controller.wechat;
 
 /**
- * Created by kongqf on 16-11-22.
+ * Created by kongqf on 16-22-22.
  */
 public class test {
+    private  String aa;
+
     public static void main(String[] args) {
-        System.out.println("111111111111");
-        System.out.println("111111111111");
-        System.out.println("111111111111");
-        System.out.println("111111111111");
-        System.out.println("111111111111");
-
-
-
-
-
-
-
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+        System.out.println("222222222222");
+        System.out.println("222222222222");
     }
 }
