3f9801134c9a06eb29eb3d66db90e7192f0b0845
wangxuan <1160880871@qq.com>
2016-11-28 17:45:32 +0800

    进入首页时，判断进入哪个页面（myMemberInfoInit.html或者myMemberInfo.html）

--- src/main/java/com/wfj/controller/wechat/CommonController.java
+++ src/main/java/com/wfj/controller/wechat/CommonController.java
@@ -2,9 +2,15 @@ package com.wfj.controller.wechat;
 
 import com.wfj.dto.AccessTokenDto;
 import com.wfj.dto.MemberInfo;
+import com.wfj.dto.MemberInfoReturnDto;
 import com.wfj.message.req.StoreInfoDto;
+import com.wfj.service.intf.MemberInfoService;
 import com.wfj.util.PropertiesUtils;
+import com.wfj.util.StringUtils;
 import com.wfj.util.WechatUtil;
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +20,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/common")
 public class CommonController {
 
+    private static Logger logger = LoggerFactory.getLogger(CommonController.class);
+
     @Autowired
     private WechatUtil util;
+
+    @Autowired
+    private MemberInfoService memberInfoService;
 
     @RequestMapping(value = "memberInfo", method = RequestMethod.GET)
     public void memberInfo(HttpServletRequest request, HttpServletResponse response,
@@ -29,10 +42,10 @@ public class CommonController {
         long starTime = System.currentTimeMillis();
         try {
             // 1 通过门店接口获取appID,appSecret
-             StoreInfoDto storeInfo = util.getStoreInfo(state);
-             System.out.println("storeInfo ================ " + storeInfo);
-            String appid =storeInfo.getAppId(); //"wx871d0104ae72e615";
-            String secret =storeInfo.getSecret(); //"00e66c2772af76181745b6f5d92b5801";
+            StoreInfoDto storeInfo = util.getStoreInfo(state);
+            System.out.println("storeInfo ================ " + storeInfo);
+            String appid = storeInfo.getAppId(); //"wx871d0104ae72e615";
+            String secret = storeInfo.getSecret(); //"00e66c2772af76181745b6f5d92b5801";
             // 2 通过appID,appSecret获取access_token
             String accessToken = util.getAccessToken(appid, secret);
             System.out.println("accessToken ================ " + accessToken);
@@ -52,7 +65,8 @@ public class CommonController {
             // Map<String, String> paramMap = new HashMap<String, String>();
             // paramMap.put("appid", storeInfo.getAppId());
             // paramMap.put("openid", atkDto.getOpenid());
-            String sendGet = getMemberInfo(appid, atkDto.getOpenid());// HttpUtil.sendGet("",
+            String sendGet = getMemberInfo(appid, atkDto.getOpenid(), storeInfo.getStoreCode());
+//            String sendGet = getMemberInfo(appid, atkDto.getOpenid());// HttpUtil.sendGet("",
             // paramMap);
             // String sendGet = getMemberInfo(storeInfo.getAppId(),
             // atkDto.getOpenid());
@@ -66,6 +80,51 @@ public class CommonController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 获取会员用户及会员卡信息(跳转页面)
+     *
+     * @param
+     * @return
+     * @throws Exception
+     */
+    private String getMemberInfo(String appid, String openid, String storeCode) throws Exception {
+        logger.info("start com.wfj.controller.wechat.CommonController.getMemberInfo(),para:{appid:" + appid + ",openid:" + openid + "}");
+        String url = "";
+        String para = "";
+        Map<String, Object> paraMap = new HashMap<String, Object>();
+        paraMap.put("appid", appid);
+        paraMap.put("openid", openid);
+        paraMap.put("storeCode", storeCode);
+        Map<String, Object> returnMap = memberInfoService.getMemberInfo(paraMap);
+        if ("0".equals(returnMap.get("code") + "")) {// 未注册、未实体绑卡 跳转至头像首页
+            logger.info("com.wfj.controller.wechat.CommonController.getMemberInfo:未注册、未实体绑卡");
+            url = PropertiesUtils.findPropertiesKey("myMemberInfoInit");
+        } else if ("1".equals(returnMap.get("code") + "")) {
+            MemberInfoReturnDto memberInfoReturnDto = new MemberInfoReturnDto();
+            BeanUtils.copyProperties(memberInfoReturnDto, returnMap.get("obj"));
+
+            String mobile = memberInfoReturnDto.getMobile();
+
+            para = "memberCode=" + memberInfoReturnDto.getMemberCode() + "&cardNo=" + memberInfoReturnDto.getCardCode() +
+                    "&custType=" + memberInfoReturnDto.getCardLevel() + "&qrcode=&mobile=" + mobile;
+
+            Integer cardType = memberInfoReturnDto.getCardType();
+            if (cardType != 1) {// 是否存在有实体卡,无实体卡
+                url = PropertiesUtils.findPropertiesKey("myMemberInfo");
+                para = para + "&entityCard=0&updatePWD=0";
+            } else if (cardType == 1) {// 有实体卡
+                url = PropertiesUtils.findPropertiesKey("myMemberInfo");
+                if (StringUtils.isNotEmpty(mobile)) {//有手机号
+                    para = para + "&unBund=0&updatePWD=0";
+                } else {//没有手机号
+                    para = para + "&unBund=0&boundPhone=0";
+                }
+            }
+        }
+        logger.info("end com.wfj.controller.wechat.CommonController.getMemberInfo(),return:" + url + para);
+        return url + para;
     }
 
 
