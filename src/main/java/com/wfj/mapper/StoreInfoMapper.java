dafde40ab876399da189766fb4f785e9e6976fc9
wangxuan <1160880871@qq.com>
2016-12-01 11:27:50 +0800

    门店查询

--- src/main/java/com/wfj/mapper/StoreInfoMapper.java
+++ src/main/java/com/wfj/mapper/StoreInfoMapper.java
@@ -1,12 +1,11 @@
 package com.wfj.mapper;
 
 import com.wfj.entity.StoreInfo;
-import com.wfj.mapper.base.BaseMapper;
 
 import java.util.List;
 import java.util.Map;
 
-public interface StoreInfoMapper extends BaseMapper {
+public interface StoreInfoMapper {
     int deleteByPrimaryKey(String sid);
 
     int insert(StoreInfo record);
