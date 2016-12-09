3ba0d0d09fd74b9cf3d8ab4320295b83dec82c63
yedong <529890739@qq.com>
2016-12-09 15:04:01 +0800

    卡券-增删改

--- src/main/java/com/wfj/controller/coupon/CouponTPLController.java
+++ src/main/java/com/wfj/controller/coupon/CouponTPLController.java
@@ -1,10 +1,24 @@
 package com.wfj.controller.coupon;
 
-import com.wfj.controller.index.BaseController;
-import com.wfj.util.Common;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wfj.annotation.SystemLog;
+import com.wfj.controller.index.BaseController;
+import com.wfj.dto.CouponTemplateDto;
+import com.wfj.entity.CouponTemplate;
+import com.wfj.entity.DataTableResult;
+import com.wfj.service.intf.CouponTemplateService;
+import com.wfj.util.Common;
 
 /**
  * CouponTPLController
@@ -16,14 +30,73 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("coupontpl")
 public class CouponTPLController extends BaseController {
 
-    @RequestMapping("getlist")
-    public String getlist(Model model) {
-        model.addAttribute("res", findByRes());
-        return Common.BACKGROUND_PATH + "/coupon/tpl/list";
-    }
+	@Autowired
+	private CouponTemplateService couponTPLService;
 
-    @RequestMapping("addUI")
-    public String addUI(Model model) {
-        return Common.BACKGROUND_PATH + "/coupon/tpl/add";
-    }
+	@RequestMapping("getlist")
+	public String getlist(Model model) {
+		model.addAttribute("res", findByRes());
+		return Common.BACKGROUND_PATH + "/coupon/tpl/list";
+	}
+
+	@RequestMapping("addUI")
+	public String addUI(Model model) {
+		return Common.BACKGROUND_PATH + "/coupon/tpl/add";
+	}
+
+	@ResponseBody
+	@RequestMapping("/findCouponTPLByPage")
+	@SystemLog(module = "卡券模板", methods = "卡券模板管理-分页查询")
+	public DataTableResult<CouponTemplate> findCouponTPLByPage(
+			@RequestBody CouponTemplateDto para) {
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("start", para.getiDisplayStart());
+		paramMap.put("limit", para.getiDisplayLength());
+		paramMap.put("ifdel", 0);
+		try {
+			BeanUtils.copyProperties(paramMap, para);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		DataTableResult<CouponTemplate> page = couponTPLService.selectPageListByParam(paramMap);
+		page.setiTotalDisplayRecords(page.getAaData().size());
+		page.setsEcho(para.getsEcho());
+		return page;
+	}
+
+	@ResponseBody
+	@RequestMapping("/addCouponTPL")
+	@SystemLog(module = "卡券模板", methods = "卡券模板管理-新增")
+	public String addCouponTPL(@RequestBody CouponTemplateDto para) {
+		CouponTemplate entity = new CouponTemplate();
+		try {
+			BeanUtils.copyProperties(entity, para);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		int i = couponTPLService.insertSelective(entity);
+		if (i == 1) {
+			return "success";
+		} else {
+			return "faile";
+		}
+	}
+
+	@ResponseBody
+	@RequestMapping("/deleteCouponTPL")
+	@SystemLog(module = "卡券模板", methods = "卡券模板管理-删除")
+	public String deleteCouponTPL(Model model) {
+		String sid = getPara("sid");
+		int del = couponTPLService.deleteByPrimaryKey(Integer.parseInt(sid));
+		if (del > 0) {
+			return "success";
+		} else {
+			return "faile";
+		}
+	}
+
 }
